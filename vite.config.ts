212bc7b107670a051061be6b43b3590de91c8294
Sarim Abbas <sarim.stays@gmail.com>
2023-01-26 15:18:38 -0800

    chane name

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/lib/index.ts"),
-      name: "cutestash",
+      name: "next-up",
       // the proper extensions will be added
       fileName: "index",
     },
