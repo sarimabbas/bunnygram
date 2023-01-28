7c40703fda95c54369be3e72a934eb45d67586c9
Sarim Abbas <sarim.stays@gmail.com>
2023-01-28 00:10:42 -0800

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
