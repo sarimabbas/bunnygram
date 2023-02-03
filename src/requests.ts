71eacd3a36b9e7c4e54e956b29d095d2d74d06c6
Sarim Abbas <sarim.stays@gmail.com>
2023-02-02 20:14:44 -0800

    utils for parsing node requests

---
+++ src/requests.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest } from "next";
+import { IncomingHttpHeaders } from "node:http";
+import type { Readable } from "node:stream";
+
+export const getRequestBody = async (req: NextApiRequest) => {
+  const buf = await buffer(req);
+  const rawBody = buf.toString("utf8");
+  const parsedBody = parseRawBody(rawBody, req.headers);
+  return {
+    rawBody,
+    parsedBody,
+  };
+};
+
+const buffer = async (readable: Readable) => {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+};
+
+const parseRawBody = (rawBody: string, headers: IncomingHttpHeaders) => {
+  try {
+    if (headers["content-type"] === "application/json") {
+      return JSON.parse(rawBody);
+    } else {
+      return {};
+    }
+  } catch {
+    return {};
+  }
+};
