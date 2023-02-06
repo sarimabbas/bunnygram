68c2cf4cc8f2132e8be1d714f30b1efe58572b2f
Sarim Abbas <sarim.stays@gmail.com>
2023-02-06 14:12:11 -0800

    wip

--- packages/bunnygram/src/index.ts
+++ packages/bunnygram/src/index.ts
@@ -1,3 +1,4 @@
+export { makeConfig } from "./scheduler/config";
 export { onReceive } from "./scheduler/receive";
 export { send } from "./scheduler/send";
 export { BasicAdapter, QStashAdapter, IAdapter } from "./adapters";
