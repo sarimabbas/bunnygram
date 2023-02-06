358ea0f3b5bd1403947933862b1a128162292f4c
Sarim Abbas <sarim.stays@gmail.com>
2023-02-06 14:29:38 -0800

    wip

--- packages/bunnygram/tsup.config.ts
+++ packages/bunnygram/tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "tsup";
-import { $ } from "zx";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   clean: true,
   format: ["esm"],
-  dts: false,
-  onSuccess: async () => {
-    $`tsc`;
-  },
+  dts: true,
+  // for some strange reason, intellisense stops working when importing
+  // bunnygram in other packages, so disabling tsc dts and using the inbuilt one
+  // for now. We lose out on declaration maps but its no big deal onSuccess:
+  // async () => { $`tsc`;
+  // },
 });
